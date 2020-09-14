a9d32c21dfc70f54e61b1d0dcb73056c86ed807f
Bernadette <bernadette.breboneria@biarri.com>
2020-09-14 08:43:40 +0800

    added mongoose, configure route to use database

--- index.js
+++ index.js
@@ -1,49 +1,63 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
     app = express();
 const port = process.env.PORT || 8000;
 
+//-- use mongoose to connect to the database
+mongoose.connect('mongodb://localhost/posts_v1', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const data = [
-    {
-        title: 'Greenest Leaves',
-        image: 'https://images.pexels.com/photos/807598/pexels-photo-807598.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-    },
-    {
-        title: 'Paradise Island',
-        image: 'https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-    },
-    {
-        title: 'Mountanous Top',
-        image: 'https://images.pexels.com/photos/2835562/pexels-photo-2835562.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
-    },
-]
+
+//-- setup schema
+const postsSchema = new mongoose.Schema({
+    title: String,
+    image: String
+})
+
+//-- save schema to a model
+const Post = new mongoose.model('Post', postsSchema);
+
+//========================ROUTE========================
+
 //-- landing page
 app.get('/', (req, res) => {
     console.log('This is the roote page!');
     res.render('landing');
 })
 
-//-- post form
+//-- posts page / index
+app.get('/posts', (req, res) => {
+    Post.find({}, function (err, allData) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(allData);
+            res.render('posts/index', { posts: allData });
+        }
+    })
+})
+
+//-- post form - for creating new posts
 app.get('/posts/new', (req, res) => {
     res.render('posts/new');
 })
 
-//-- posts page / index
-app.get('/posts', (req, res) => {
-    res.render('posts/index', { posts: data });
-})
-
 //-- post route
 app.post('/posts', (req, res) => {
-    // get data from form and add to post array
-    data.push(req.body);
-    // redirect to posts page
-    res.redirect('/posts');
+    //-- create a new post
+    Post.create(req.body, function (err, newlyCreated) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(newlyCreated)
+            //-- redirect to posts route
+            res.redirect('/posts');
+        }
+    })
 })
 
 
